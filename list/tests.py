40de754e15840fcc333c17cd0ec01daf3ea687fc
Pawe Szabla <pawelszabla@gmail.com>
2021-10-09 23:25:21 +0200

    html into template, test into non constant

--- list/tests.py
+++ list/tests.py
@@ -3,6 +3,7 @@ from django.test import TestCase
 from list.views import home_page
 from django.http import HttpRequest
 from django.http import HttpResponse
+from django.template.loader import render_to_string
 # Create your tests here.
 
 class HomePageTest(TestCase):
@@ -14,6 +15,8 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        self.assertTrue(response.content.startswith(b'<html>'))
-        self.assertIn(b'<title>Listy rzeczy do zrobienia</title>', response.content)
-        self.assertTrue(response.content.endswith(b'</html>'))
+        expected_html = render_to_string('home.html')
+        self.assertEqual(response.content.decode(),expected_html)
+        # self.assertTrue(response.content.startswith(b'<html>'))
+        # self.assertIn(b'<title>Listy rzeczy do zrobienia</title>', response.content)
+        # self.assertTrue(response.content.strip().endswith(b'</html>'))
