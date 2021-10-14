5a85477506b38cb4d36c6cb4c80afd2c40499849
Pawe Szabla <pawelszabla@gmail.com>
2021-10-14 20:57:37 +0200

    test

--- list/tests.py
+++ list/tests.py
@@ -1,6 +1,7 @@
 from django.urls import resolve
 from django.test import TestCase
 from list.views import home_page
+from list.models import Item
 from django.http import HttpRequest
 from django.http import HttpResponse
 from django.template.loader import render_to_string
@@ -30,3 +31,22 @@ class HomePageTest(TestCase):
         self.assertIn('Nowy element listy', response.content.decode())
         expected_html = render_to_string('home.html',{'new_item_text': 'Nowy element listy'})
         self.assertEqual(response.content.decode(),expected_html)
+
+class ItemModelTest(TestCase):
+
+    def test_saving_and_retriving_items(self):
+        first_item=Item()
+        first_item.text='Absolutnie pierwszy element listy'
+        first_item.save()
+
+        second_item=Item()
+        second_item.text = 'Drugi element listy'
+        second_item.save
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(),2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text,'Absolutnie pierwszy element listy')
+        self.assertEqual(second_saved_item.text,'Drugi element listy')
