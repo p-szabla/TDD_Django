6bf55c0546ae16d66e44a8f679e46bfc271de2ad
Pawe Szabla <pawelszabla@gmail.com>
2021-10-14 20:32:33 +0200

    functional test

--- functional_test.py
+++ functional_test.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
-
+import time
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -11,6 +11,12 @@ class NewVisitorTest(unittest.TestCase):
 
     def tearDown(self):
         self.browser.quit()
+
+    def check_for_row_in_list_table(self,row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+
+        self.assertIn(row_text,[row.text for row in rows])
 
     def test_can_start_a_list_and_retreive_it_later(self):
         self.browser.get('http://localhost:8000')
@@ -24,20 +30,22 @@ class NewVisitorTest(unittest.TestCase):
 
         inputbox.send_keys('kupić pawie pióra')
         inputbox.send_keys(Keys.ENTER)
-        self.browser.implicitly_wait(3)
-        # inputbox.send_keys('zrobic przynety')
-        # inputbox.send_keys(Keys.ENTER)
-        # import time
-        # time.sleep(10)
+        time.sleep(10)
+        self.check_for_row_in_list_table('1: kupić pawie pióra')
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('zrobic przynety')
+        inputbox.send_keys(Keys.ENTER)
+        # self.browser.implicitly_wait(3000)
+
+        time.sleep(10)
         #
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: kupić pawie pióra', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: kupić pawie pióra')
+        self.check_for_row_in_list_table('2: zrobic przynety')
 
-        # self.assertIn('1: kupić pawie pióra',[row.text for row in rows])
-        # self.assertIn('2: zrobic przynety',[row.text for row in rows])
-        # self.fail('zakończenie testu')
 
 
 if __name__ == '__main__':
