1d59e1acab36016e510db95bee74e884d26d38be
Pawe Szabla <pawelszabla@gmail.com>
2021-10-27 18:54:14 +0200

    to fix

--- list/tests.py
+++ list/tests.py
@@ -85,14 +85,26 @@ class ListAndItemModelTest(TestCase):
 
 class ListViewTest(TestCase):
 
-    def test_diplays_all_items_list(self):
-        list_ = List.objects.create()
-        Item.objects.create(text='itemey 1',list=list_)
-        Item.objects.create(text='itemey 2',list=list_)
-        request = HttpRequest()
-        response = self.client.get('/lists/the-only-list-in-the-world')
+    def test_uses_list_templates(self):
+        list_=List.objects.create()
+        response = self.client.get(f'/list/{list_.id}/' )
+        self.assertTemplateUsed(response,'list.html')
+
+    def test_diplays_only_items_for_list(self):
+        correct_list = List.objects.create()
+        Item.objects.create(text='itemey 1',list=correct_list)
+        Item.objects.create(text='itemey 2',list=correct_list)
+
+        other_list = List.objects.create()
+        Item.objects.create(text='itemey a',list=other_list)
+        Item.objects.create(text='itemey b',list=other_list)
+
+        response = self.client.get(f'/list/{correct_list.id}/')
         self.assertContains(response, 'itemey 1')
         self.assertContains(response, 'itemey 2')
+        self.assertNotContains(response, 'itemey a')
+        self.assertNotContains(response, 'itemey b')
+
 
     def uses_list_template(self):
         response = self.client.get('/lists/the-only-lists-in-the-world')
--- superlist/urls.py
+++ superlist/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
     path('', views.home_page,name='home'),
     path('lists/the-only-list-in-the-world', views.view_list,name='view_list'),
     path('lists/new', views.new_list,name='new_list'),
+    path('lists/(.+)/', views.view_list,name='view_list'),
      # url(r'^$', views.home_page, name='home'),
     # path('admin/', admin.site.urls),
 ]
