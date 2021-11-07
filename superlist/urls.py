75f0f4de58af5e283a20b4cca02b4b5a5181e099
Pawe Szabla <pawelszabla@gmail.com>
2021-11-07 17:07:05 +0100

    end ch6

--- superlist/urls.py
+++ superlist/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('lists/new', views.new_list,name='new_list'),
     # path('lists/<id>/', views.view_list,name='view_list'),
     re_path(r'^lists/(\d+)/$',views.view_list,name='view_list'),
-    re_path(r'^lists/(\d+)/add_item$',views.add_item,name='add_item')
+    re_path(r'^lists/(\d+)/add_item$',views.add_item,name='add_i')
      # url(r'^$', views.home_page, name='home'),
     # path('admin/', admin.site.urls),
 ]
