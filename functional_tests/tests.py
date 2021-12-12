a7c8dacf75fda8fc4d9cea7dfcef9bcc399d1477
Pawe Szabla <pawelszabla@gmail.com>
2021-12-12 09:40:59 +0100

    chapert 8

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -2,13 +2,30 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
 import time
-
+import sys
 # from django.test import LiveServerTestCase
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
 
 # class NewVisitorTest(LiveServerTestCase):
 class NewVisitorTest(StaticLiveServerTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        for arg in sys.argv:
+            if 'liveserver' in arg:
+                cls.server_url = 'htp://'+arg.split('=')[1]
+                return
+        super().setUpClass()
+        cls.server_url=cls.live_server_url
+
+    @classmethod
+    def tearDownClass(cls):
+        if cls.server_url == cls.live_server_url:
+            super().tearDownClass()
+
+
+
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
@@ -23,7 +40,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.assertIn(row_text,[row.text for row in rows])
 
     def test_can_start_a_list_and_retreive_it_later(self):
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         self.assertIn('Listy', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('lista',header_text)
@@ -55,7 +72,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
         self.browser.quit()
         self.browser = webdriver.Firefox()
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('kupić pawie pióra',page_text)
         self.assertNotIn('zrobic przynety',page_text)
@@ -73,7 +90,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.assertIn('kupić mleko',page_text)
 
     def test_layout_and_styling(self):
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         self.browser.set_window_size(1024, 768)
 
         inputbox = self.browser.find_element_by_id('id_new_item')
